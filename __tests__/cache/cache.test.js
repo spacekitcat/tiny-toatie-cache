359b41bb7832b781615c48e76d50025f3eb742fb
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2019-03-03 21:47:02 +0000

    feat(store): Adding basic append and buffe read code via cloakroom

--- __tests__/cache/cache.test.js
+++ __tests__/cache/cache.test.js
@@ -1,7 +1,17 @@
 import Cache from '../../src/cache/cache';
 
-describe('The `doCacheLookup` function', () => {
-    it('runs', () => {
-        new Cache();
+describe('The `Cache` class', () => {
+  it('append method', () => {
+    const sut = new Cache();
+    const expectedAppendList = [0x45, 0x44, 0x46];
+    sut.append(Buffer.from(expectedAppendList));
+    expect(sut.getReadOnlyBuffer()).toMatchObject(expectedAppendList);
+  });
+    
+  it('append method (alt)', () => {
+    const sut = new Cache();
+    const expectedAppendList = [0x75, 0x64, 0x26];
+    sut.append(Buffer.from(expectedAppendList));
+    expect(sut.getReadOnlyBuffer()).toMatchObject(expectedAppendList);
   });
 });
--- src/cache/cache.js
+++ src/cache/cache.js
@@ -5,6 +5,14 @@ class Cache {
     this.offset = 0;
     this.internalCache = new Proxy();
   }
+
+  append(list) {
+    this.internalCache.append(list)
+  }
+
+  getReadOnlyBuffer() {
+    return this.internalCache.getReadOnlyBuffer();
+  }
 }
 
 export default Cache;
