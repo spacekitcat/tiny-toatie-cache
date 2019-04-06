29e897b34416a3854b956d295e55e5d0a2176a11
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2019-04-06 18:31:51 +0100

    feat(dispatch): Adding dispatch handler naive implementation.

--- __tests__/cache/lookup-dispatcher.spec.js
+++ __tests__/cache/lookup-dispatcher.spec.js
@@ -1,8 +1,67 @@
 import LookupDispatcher from '../../src/cache/lookup-dispatcher';
+import CacheStore from '../../src/cache-store';
+
+const createStoreInstance = contents => {
+  const store = new CacheStore();
+  store.append(contents);
+  return store;
+};
 
 describe('The `lookupDispatch` module', () => {
-  it('should be able to instantiate', () => {
-    const lookupDispatch = new LookupDispatcher();
-    expect(lookupDispatch).not.toBe(null);
+  describe('when no handlers are registered', () => {
+    it('should return null', () => {
+      const event = {
+        store: createStoreInstance(Buffer.from([0x22, 0x43, 0x87])),
+        lookupKey: Buffer.from([0x43, 0x87])
+      };
+
+      const lookupDispatch = new LookupDispatcher();
+      expect(lookupDispatch.handleLookup(event)).toBe(null);
+    });
+  });
+
+  describe('when one handler is registered', () => {
+    it('should call the handler', () => {
+      const event = {
+        store: createStoreInstance(Buffer.from([0x22, 0x43, 0x87])),
+        lookupKey: Buffer.from([0x43, 0x87])
+      };
+
+      const handler = jest.fn();
+      const lookupDispatch = new LookupDispatcher();
+      lookupDispatch.registerHandler(handler);
+
+      lookupDispatch.handleLookup(event);
+
+      expect(handler).toHaveBeenCalledWith(event);
+    });
+
+    describe('when the event cannot be handled', () => {
+      it('should return null', () => {
+        const handler = jest.fn();
+        handler.mockImplementation(() => null);
+        const lookupDispatch = new LookupDispatcher();
+        lookupDispatch.registerHandler(handler);
+
+        expect(lookupDispatch.handleLookup({})).toBe(null);
+      });
+    });
+
+    describe('when the event can be handled', () => {
+      it('should return the handler response', () => {
+        const mockResponse = {
+          offset: 1,
+          length: 2,
+          value: Buffer.from([0x22])
+        };
+
+        const handler = jest.fn();
+        handler.mockImplementation(() => mockResponse);
+        const lookupDispatch = new LookupDispatcher();
+        lookupDispatch.registerHandler(handler);
+
+        expect(lookupDispatch.handleLookup({})).toMatchObject(mockResponse);
+      });
+    });
   });
 });
--- src/cache/lookup-dispatcher.js
+++ src/cache/lookup-dispatcher.js
@@ -1,3 +1,27 @@
-class LookupDispatcher {}
+class LookupDispatcher {
+  constructor() {
+    this.handlers = [];
+  }
+
+  handleLookup(event) {
+    if (this.handlers.length < 1) {
+      return null;
+    }
+
+    for (let i = 0; i < this.handlers.length; ++i) {
+      const handlerFn = this.handlers[i];
+      const result = handlerFn(event);
+      if (result) {
+        return result;
+      }
+    }
+
+    return null;
+  }
+
+  registerHandler(handlerFn) {
+    this.handlers.push(handlerFn);
+  }
+}
 
 export default LookupDispatcher;
